9381c70c06fbb5d9842d8b4f399299c4ae60fb2d
yeetproudction <98375919+yeetproudction@users.noreply.github.com>
2022-10-10 16:25:05 -1000

    add git.nore and datab\base.js
    I added database supabase\database.js and a .gitignore

---
+++ database supabase/database.js
@@ -0,0 +1,9 @@
+const { SUPABASE_KEY } = require('./Private/SUPABASE_KEY.js');
+const Supabase = require('@supabase/supabase-js');
+const supabaseUrl = 'https://dxlakcttkbzffaoyanvn.supabase.co';
+const supabaseKey = SUPABASE_KEY;
+const supabase = Supabase.createClient(supabaseUrl, supabaseKey);
+
+exports.supabase = {
+  supabase
+}
